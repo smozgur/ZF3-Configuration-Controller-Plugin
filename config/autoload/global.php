a08a35bbd4db2b9c21bdbe13de9c95cdfddec53a
Suat Ozgur <smozgur@gmail.com>
2016-07-04 14:29:22 +0300

    Create global.php

---
+++ config/autoload/global.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Settings to be used in the controllers application wide 
+ */
+return [
+    'application' => [
+        'settings' => [
+            'pagination'    => [
+                'itemsPerPage' => 13,
+            ],
+        ],
+    ],
+];
